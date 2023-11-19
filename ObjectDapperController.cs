f89e01a29a9fb032f89d2a63eb6f60e3994fca5e
Stofi09 <kristof.solak@gmail.com>
2023-11-19 16:37:24 +0100

    first init

---
+++ ObjectDapperController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace WebApplication4
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ObjectDapperController : ControllerBase
+    {
+        private readonly YourDataAccessClass dataAccess;
+
+        public ObjectDapperController(YourDataAccessClass dataAccess)
+        {
+            this.dataAccess = dataAccess;
+        }
+
+        [HttpPost("BulkInsertTest")]
+        public IActionResult BulkInsertTest()
+        {
+            try
+            {
+                // Create 500,000 anonymous objects
+                List<ObjectDapperDTO> objectsToInsert = new List<ObjectDapperDTO>();
+                for (int i = 0; i < 1000000; i++) // 89026
+                {
+                    objectsToInsert.Add(new ObjectDapperDTO
+                    {
+                        Name = $"Name{i}",
+                        Description = $"Description{i}"
+                    });
+                }
+                Stopwatch sw = Stopwatch.StartNew();
+                // Perform the bulk insert
+                dataAccess.BulkInsertObjects(objectsToInsert);
+                sw.Stop();
+                long number = sw.ElapsedMilliseconds;
+                return Ok(number);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ YourDataAccessClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper.Contrib.Extensions;
+using WebApplication4;
+
+public class YourDataAccessClass
+    {
+        
+
+        public YourDataAccessClass()
+        {
+        }
+
+        public void BulkInsertObjects(List<ObjectDapperDTO> objects)
+        {
+            using (IDbConnection dbConnection = new SqlConnection("server=(localdb)\\MSSQLLocalDB; database=YourDatabaseName; Integrated Security=true"))
+            {
+                dbConnection.Open();
+
+                using (var transaction = dbConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        dbConnection.Insert(objects, transaction);
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+
